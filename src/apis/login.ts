70b8a37ced097a3d41c70a06f3b84194bcedd958
georgina6 <lunarecho530@gmail.com>
2023-09-22 18:35:59 +0800

    initial login

---
+++ src/apis/login.ts
@@ -0,0 +1,10 @@
+import { $http } from '.';
+
+// 登录
+export const loginApi = (data: { password: string; username: string }) => {
+    return $http({
+        method: 'POST',
+        url: '/admin/login',
+        data,
+    })
+}
---
+++ src/views/LoginView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="login">init</div>
+</template>
+
+<stript setup lang="ts">
+
+</stript>
+
+<style scope>
+#login {
+  height: 100vh;
+  background: linear-gradient(to right, #1960ae, #025291);
+}
+</style>
